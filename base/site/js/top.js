972ef3daa79d00c6b7229b460da9cd72a9e4f406
BASARA0826 <clearheart260@gmail.com>
2024-05-24 12:31:20 +0900

    UPDATE

--- base/site/js/top.js
+++
@@ -1,7 +0,0 @@
-$(".slider").slick({
-    autoplay: true, // 自動再生
-    arrows: true, // 矢印
-    dots: true, // インジケーター
-    prevArrow: '<img src="images/slick_prevI.png" class="prev-arrow">',
-    nextArrow: '<img src="images/slick_nextI.png" class="next-arrow">',
-  });
