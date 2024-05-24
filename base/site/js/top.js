0b524ab9a536a0bc8fd7a2c506eb334f1e107223
Weru7e <r-tomipc@outlook.jp>
2024-05-24 09:56:18 +0900

    topかんせい

---
+++ base/site/js/top.js
@@ -0,0 +1,7 @@
+$(".slider").slick({
+    autoplay: true, // 自動再生
+    arrows: true, // 矢印
+    dots: true, // インジケーター
+    prevArrow: '<img src="images/slick_prevI.png" class="prev-arrow">',
+    nextArrow: '<img src="images/slick_nextI.png" class="next-arrow">',
+  });
